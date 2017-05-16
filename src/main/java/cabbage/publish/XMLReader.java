8dd126775d6569533408f6398558e42294446f8d
endend20000 <end.2000@163.com>
2017-05-16 10:31:44 +0800

    测试用例

--- src/main/java/cabbage/publish/XMLReader.java
+++ src/main/java/cabbage/publish/XMLReader.java
@@ -9,6 +9,7 @@ import javax.xml.bind.Unmarshaller;
 public class XMLReader {
 	public static List<Project> getProjects() throws Exception{
         //File file = new File( "d:\\service.xml" );
+		//File file = new File(System.getProperty("user.dir")+"/service.xml");
 		File file = new File( "src/main/java/service.xml" );
         JAXBContext jaxbContext = JAXBContext.newInstance( Projects.class );
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
